046a5a7928d8c9552f73c9c341a6cc38f5024818
D.Leap <dilipkumar2000.r@gmail.com>
2026-01-19 09:30:56 +0530

    fixed manim pipeline. you reading this have no idea how hard this was. next : enhance rag, add user db then aws.

--- backend/llm.py
+++ backend/llm.py
@@ -1,58 +1,124 @@
+from huggingface_hub import InferenceClient
 from google import genai
 import subprocess
+import json
 import os
 import sys
-import uuid
+from pydantic import BaseModel, ValidationError, constr
 from dotenv import load_dotenv
 load_dotenv()
 
+class ModelResponse(BaseModel):
+    code:constr(min_length=1, strip_whitespace=True)
+    className:str
 
-def inferModel(prompt:str)->str:
-    data = """
-        generate the code for a manim video for linear regression working, idk anything about it i need you to elaborate and teach me.
+# now this can be passes as a response format to the model, to ensure that it returns without any backticks or extra bullshit
+response_format = {
+    "type":"json_schema",
+    "json_schema":{
+        "name" : "Model_Response",
+        "schema":ModelResponse.model_json_schema(),
+        "strict":True
+    }
+}
+
+def inferModel(input:str)->str:
+    #genai.client
+    client = InferenceClient(api_key=os.getenv("HUGGINGFACEHUB_API_TOKEN")) 
+    prompt= f"""{input}
+        YOU ARE A DEVELOPER WHOSE TASK IS TO GENERATE MANIM CODE THAT WILL FURTHER BE RUN USING `manim -pql` COMMAND, 
+        THE SYSTEM MESSAGE FOR THE CODE GENERATION IS AS FOLLOWS YOU ARE TO STRICTLY ADHERE TO EVERY SINGLE RULE IN THIS
+        You MUST return a valid JSON object with this EXACT structure:
+            {{
+                "code": "escaped python code here with \\n for newlines and \\\" for quotes",
+                "className": "YourClassName"
+            }}
+        ** rules for illustration ** these rules apply only for the case when a video / illustration is being requested: 
+            * divide the task into 4 parts each occupying 5s of the time, in the end add a last chunk of 5s to complete the video in about 20seconds time 
+            * each video must be elaborate and must have as much information about the user's query as possible, conveying the information in a clean and crisp manner without any deviation from the inital query. 
+            * IN THE CASE WHEN A USER'S QUERY CAN NOT BE ILLUSTRATED, ADD TEXT DESCRIBING THE RESPONSE AND DRAW A FLOWCHART OR ANY STATIC DIAGRAM IN NECESSARY 
+            * BY ALL MEANS TRY TO GENERATE A VIDEO IN UNDER 25 SECONDS, ANYTHING BEYOND THAT WILL RESULT IN A -50 POINT LOSS IN YOUR REWARDS.
+        ** strict rules: ** 
+        -- MAKE SURE THAT ALL THE CODE YOU WRITE EXISTS IN ONE FILE, ALL THE HELPER FUNCTIONS IF USED OR NEEDED MUST BE INCLUDED IN THE SAME SCRIPT.
+        -- THE SCRIPT WILL BE RUN IN AN EVIRONMENT WITH THE LATEST VERSION OF MANIM AND YOU MUST CREATE A SCRIPT THAT IS VALID ACCORDINGLY.
+        -- YOU HAVE TO PROVIDE THE CODE ALWAYS FOR THE ILLUSTRATIONS IRRESPECTIVE OF THE COMPLEXITY.
+        --  ENSURE THAT THE SYNTAX IS TAKEN CARE OF, THERE MUST STRICTLY BE NO SYNTAX ERRORS. EXPECIALLY WITH STRINGS QUOTATIONS CLOSING.
+        -- For all illustrations return an two items, first being the actual code for the illustration,
+            the second is the name of the illustration class in the end, they need to be separated like this : 
+            ..manimcode.. --className-- IllustrationClassName (replace this with the class from the code, this must have a --className-- text before it). 
+        -- Every step in the illustration must have a title, a title that changes and corresponds to the context of the illustration, the title must be simple to read and shouldnt exceed 10 charecters. 
+        -- DO NOT EMBED THE CODE IN DOCSTRING QUOTES AT THE BEGINING OR AT THE END, ONLY IMPORTS, CLASS FOR THE MANIM ILLUSTRATION IS EXPECTED IN THE RESPONSE.
+        -- For all the illustrations dont create a response with 'python' at the top to specify or any other indicative words that may ruin the script run.
+        -- Always create a script alone with its necessary imports, it will be run with the manim -pwm flag, work accordingly to not add any extra charecters that will result in an error.
+        -- strictly must have only one video of the entire illustration, the code must follow the above rules and not be followed by a summary or any thing apart from just the code.
+        -- the response mut only contrain the code, and no other output tokens, never wrap the code in any docstring or language name that directly results in an error when manim command is run
+        including but not limited to greetings or un necessary special charecters or any explanation of the code.
     """
-    client = genai.Client() #add your api key here. but dont fucking leak it like an idiot
-    response = client.models.generate_content(
-        model="gemini-3-flash-preview",
-        contents=f"""{prompt}
-            ** rules for illustration **
-            these rules apply only for the case when a video / illustration is being requested:
-                * divide the task into 4 parts each occupying 5s of the time, 
-                    in the end add a last chunk of 5s to complete the video in about 20seconds time
-                * each video must be elaborate and must have as much information about the user's query as possible,
-                  conveying the information in a clean and crisp manner without any deviation from the inital query.
-                * IN THE CASE WHEN A USER'S QUERY CAN NOT BE ILLUSTRATED, ADD TEXT DESCRIBING THE RESPONSE AND DRAW A FLOWCHART OR ANY STATIC DIAGRAM IN NECESSARY
-                * BY ALL MEANS TRY TO GENERATE A VIDEO IN UNDER 25 SECONDS, ANYTHING BEYOND THAT WILL RESULT IN A -50 POINT LOSS IN YOUR REWARDS.
-            ** strict rules: **
-                -- For all illustrations return an two items, first being the actual code for the illustration, the second is the name of the illustration class in the end, 
-                    they need to be separated like this : ..manimcode..  --className--  NeuralNetworkIllustration
-                -- For all the illustrations dont create a response with 'python' at the top to specify or any other indicative words that may ruin the script run.
-                -- Always create a script alone with its necessary imports, it will be run with the manim -pwm flag, work accordingly
-                -- strictly must have only one video of the entire illustration
-                -- the response mut only contrain the code, and no other output tokens,
-                including but not limited to greetings or un necessary special charecters or any explanation of the code.
-                -- Every step in the illustration must have a title, a title that changes and corresponds to the context of the illustration
-        """,
+    messages=[
+        {
+            "role":"system",
+            "content":"You are a manim developer, you are tasked to strictly return only code for manim illustrations in the form of the given json response format"
+        },
+        {
+            "role": "user",
+            "content": prompt
+        },
+    ]
+    response = client.chat_completion(
+        model="zai-org/GLM-4.7",
+        messages= messages,
+        max_tokens=3000,
+        temperature=0.7,
+        response_format=response_format
     )
-    className = response.text.split('--className--')
-    print(className)
-    return className
+    raw = response.choices[0].message.content
+    if not raw.startswith("{"):
+        raise RuntimeError("Model did not return JSON:\n" + raw)
+    try:
+        parsed = json.loads(raw)
+    except json.JSONDecodeError:
+        cleaned = raw.strip()
+
+        for fence in ["```", "```python", "```py"]:
+            cleaned = cleaned.replace(fence, "")
+
+        # if cleaned.startswith('"') and cleaned.endswith('"'): # just incase its embedded in quotes
+        #     cleaned = cleaned[1:-1]
+
+        parsed = json.loads(cleaned)
+
+
+    try:
+        response_text = ModelResponse(**parsed)
+    except ValidationError as e:
+        print(e.message)
+        print(response_text)
+
+    print(response_text.className)
+    return response_text
 
 if __name__ == "__main__":
-    prompt = """how does the alphabets go? use an example for each alphabet with both capital and lowercase letters"""
-    res= inferModel(prompt=prompt) 
-    id = uuid.uuid4()
-    gen = os.path.join(os.path.dirname(__name__), 'generated-scripts',res[1]+'.py') # add the session id as the joining name next
-    if len(res[0]) > 10:
+    prompt = """lord ganesha entering the atmosphere, make him simply a stick figure but add
+      all the effects that disturb the cosmos and remove all bads from the planet"""
+    
+    res= inferModel(input=prompt) 
+    print(type(res))
+    # print(len(res))
+    print(res.className)
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    gen_dir = os.path.join(base_dir, "generated-scripts")
+
+    os.makedirs(gen_dir, exist_ok=True)
+
+    gen = os.path.join(gen_dir, f"{res.className}.py")
+    # add the session id as the joining name next
+    if len(res.code) > 10:
         print('writing to a file')
-        print(res[1])
         with open (gen, 'w') as f:
-            f.write(res[0])
-    file = os.path.abspath(f"generated-scripts/{res[1]}.py")
+            f.write(res.code)
+    file = os.path.abspath(f"generated-scripts/{res.className}.py")
     subprocess.run(
-        [sys.executable, "-m", "manim", "-pql", file, res[1]],
+        [sys.executable, "-m", "manim", "-pql", file, res.className],
         check=True
     )
-        # os.system(f".venv/bin/python {res[1]}.py") # runs the python file
-        # use subprocess to run the python file
     print(res)
--- backend/server.py
+++ backend/server.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, render_template, jsonify
 from flask_cors import CORS 
 from llm import inferModel
-from rag import stores
+from rag import RagAgent
 app = Flask(__name__)
 CORS(app, resources={
     r'/*':{'origin':'http://localhost:5500'}})
@@ -21,7 +21,7 @@ def home():
     return '<p>200,SERVER UP AND RUNNING.</p>'
 
 allowed_files = ['pdf','xlsx','docx']
-@app.route('/rag', methods=['GET','POST']) # post req to receive the files (1) for rag with the prompt, and send the response back.
+@app.route('/upload', methods=['GET','POST']) # post req to receive the files (1) for rag with the prompt, and send the response back.
 def rag():
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -46,8 +46,23 @@ def rag():
     else:
         return 'GET the fuck out, invalid request method.', 403
 
-            
-        
+
+@app.route('/response', methods=['GET'])
+def rag_response(): 
+    """
+        returns the response that is generated from RAG class 
+        todo : add special methods in rag class that generates the response, 
+        then return that response form this endpoint  
+    """
+    llm = HuggingFaceEndpoint( # add your huggingface token, this shit free and good heck yeah!
+        repo_id= "Qwen/Qwen2.5-7B-Instruct",
+        temperature = 0.7,
+    )
+    model = ChatHuggingFace(llm=llm)
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+    rag = RagAgent(embeddings=embeddings, model=model)
+    rag.store()
+    
       
 
 if __name__ =="__main__":
